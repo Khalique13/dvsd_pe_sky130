ab7f7207b1f3a4d07041877494004630af353569
Mohammad Khalique Khan <gjkhalique@gmail.com>
2021-10-16 00:19:20 +0530

    Add files via upload

---
+++ dvsd_pe/config.tcl
@@ -0,0 +1,25 @@
+# User config
+set ::env(DESIGN_NAME) dvsd_pe
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
+
+# turn off clock
+set ::env(CLOCK_TREE_SYNTH) 0
+set ::env(CLOCK_PORT) ""
+
+set ::env(PL_SKIP_INITIAL_PLACEMENT) 1
+set ::env(PL_RANDOM_GLB_PLACEMENT) 0
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 50 50"
+set ::env(PL_TARGET_DENSITY) 0.75
+
+set ::env(FP_HORIZONTAL_HALO) 6
+set ::env(FP_VERTICAL_HALO) $::env(FP_HORIZONTAL_HALO)
+
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
